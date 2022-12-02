6b2f503edef4727b0ccf9dd57957b7c156cd4192
LuongLeVan <levanluong6a8@gmail.com>
2022-12-02 14:53:17 +0700

    fa

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-const CLIENT_URL = "https://ecommerce-demo-bice-delta.vercel.app/";
+const CLIENT_URL = "https://ecommerce-demo-bice-delta.vercel.app";
 
 router.get("/login/success", (req, res) => {
   if (req.user) {
